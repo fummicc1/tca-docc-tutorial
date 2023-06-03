97fab190605ad4fbf83bcd71766ba1d00958517c
fummicc1 <fumiya.tennis1@gmail.com>
2023-06-03 23:10:19 +0900

    Initial Commit

---
+++ TcaTutorial/TcaTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcaTutorialApp.swift
+//  TcaTutorial
+//
+//  Created by Fumiya Tanaka on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct TcaTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
